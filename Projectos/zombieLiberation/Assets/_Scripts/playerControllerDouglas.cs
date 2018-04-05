4280d4e037dd5b1001b62add9fe6fed64d8e77cc
Ninja Nazal <euricomartinss@gmail.com>
2018-04-05 15:59:53 +0100

    update

---
+++ Projectos/zombieLiberation/Assets/_Scripts/playerControllerDouglas.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControllerDouglas : MonoBehaviour
+{
+
+    private Animator playerAnim;
+    private float idleAnim, targetIdle;
+
+    private float animationTimer;
+
+    // Use this for initialization
+    void Start()
+    {
+        //animator
+        playerAnim = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        // idle
+        if (!playerAnim.GetBool("isfighting") && !playerAnim.GetBool("isMoving"))
+        {
+            if (idleAnim >= targetIdle)
+                
+
+            idleAnim = Mathf.Lerp(idleAnim, targetIdle, 0.001f);
+            playerAnim.SetFloat("randIdle", idleAnim);
+            Debug.Log(idleAnim + "," + targetIdle);
+        }
+
+
+
+    }
+}
