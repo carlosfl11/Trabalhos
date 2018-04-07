7f5792aaabba975d19feb0841c3c1b87359e414b
Ninja Nazal <euricomartinss@gmail.com>
2018-04-07 19:00:35 +0100

    update controllerScript

--- Projectos/zombieLiberation/Assets/_Scripts/playerControllerDouglas.cs
+++ Projectos/zombieLiberation/Assets/_Scripts/playerControllerDouglas.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class playerControllerDouglas : MonoBehaviour
 {
     public float acelaration, angularVelocity;
+
+    private float actualAVel;
     private Animator playerAnim;
     private float idleAnim, changevalue;
 
@@ -14,6 +16,7 @@ public class playerControllerDouglas : MonoBehaviour
     {
         //animator
         playerAnim = GetComponent<Animator>();
+        actualAVel = angularVelocity;
 
     }
 
@@ -34,22 +37,28 @@ public class playerControllerDouglas : MonoBehaviour
         // move
         if (Input.GetAxis("Vertical") > 0)
         {
-
+            //walking forward
             playerAnim.SetBool("isMoving", true);
+            // turn faster when wallking
+            actualAVel = angularVelocity * 2;
+
             //run
             if (Input.GetKey(KeyCode.LeftShift))
             {
+                // turn velocity return to base value
+                actualAVel = angularVelocity;
+                // blend transition to running
                 if (playerAnim.GetFloat("LocX") < 1.0f)
                     playerAnim.SetFloat("LocX", playerAnim.GetFloat("LocX") + acelaration * Time.deltaTime);
             }
             else if (playerAnim.GetFloat("LocX") > 0.0f)
                 playerAnim.SetFloat("LocX", playerAnim.GetFloat("LocX") - acelaration * Time.deltaTime);
-            
+
             // turn while moving
             if (Input.GetAxis("Horizontal") > 0)
-                transform.Rotate(transform.up, angularVelocity * Time.deltaTime);
+                transform.Rotate(transform.up, actualAVel * Time.deltaTime);
             else if (Input.GetAxis("Horizontal") < 0)
-                transform.Rotate(transform.up, -angularVelocity * Time.deltaTime);
+                transform.Rotate(transform.up, -actualAVel * Time.deltaTime);
 
         }
         else
