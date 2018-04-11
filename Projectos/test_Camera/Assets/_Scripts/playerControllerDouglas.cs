f9dc1ece93ec864d52ef58e76a7c73020aa6dada
Ninja Nazal <euricomartinss@gmail.com>
2018-04-11 15:59:31 +0100

    updateAnimations@testCam

--- Projectos/test_Camera/Assets/_Scripts/playerControllerDouglas.cs
+++ Projectos/test_Camera/Assets/_Scripts/playerControllerDouglas.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class playerControllerDouglas : MonoBehaviour
 {
-    //Rotation
-    public float acelaration, angularVelocity;
-    private float actualAVel;
-
+   
     //Animation
     private Animator playerAnim;
     private float idleAnim, changevalue;
@@ -26,7 +23,6 @@ public class playerControllerDouglas : MonoBehaviour
         cameraTarget = GameObject.Find("CameraTarget").transform;
 
         playerAnim = GetComponent<Animator>();
-        actualAVel = angularVelocity;
 
         //Camera
         cameraPositionTarget = transform.position - (transform.forward * CameraDistance) + (transform.up * CameraHeight);
@@ -38,7 +34,7 @@ public class playerControllerDouglas : MonoBehaviour
     void Update()
     {
         // idle
-        if (!playerAnim.GetBool("isfighting") && !playerAnim.GetBool("isMoving"))
+        if (!playerAnim.GetBool("isMoving"))
         {
             if (idleAnim <= 0)
                 changevalue = 0.001f;
@@ -55,26 +51,7 @@ public class playerControllerDouglas : MonoBehaviour
             //walking forward
             playerAnim.SetBool("isMoving", true);
             // turn faster when wallking
-            actualAVel = angularVelocity * 2;
-
-            //run
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                // turn velocity return to base value
-                actualAVel = angularVelocity;
-                // blend transition to running
-                if (playerAnim.GetFloat("LocX") < 1.0f)
-                    playerAnim.SetFloat("LocX", playerAnim.GetFloat("LocX") + acelaration * Time.deltaTime);
-            }
-            else if (playerAnim.GetFloat("LocX") > 0.0f)
-                playerAnim.SetFloat("LocX", playerAnim.GetFloat("LocX") - acelaration * Time.deltaTime);
-
-            // turn while moving
-            if (Input.GetAxis("Horizontal") > 0)
-                transform.Rotate(transform.up, actualAVel * Time.deltaTime);
-            else if (Input.GetAxis("Horizontal") < 0)
-                transform.Rotate(transform.up, -actualAVel * Time.deltaTime);
-
+            
         }
         else
             playerAnim.SetBool("isMoving", false);
