74e3fa4f818904ca80d7e057a30ac63bfbe4ee13
Ninja Nazal <euricomartinss@gmail.com>
2018-04-21 01:02:10 +0100

    betterCam & move

---
+++ Projectos/testMechanics_Proto/Assets/_scripts/cameraController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+
+
+    public GameObject objToFollow;
+    public float camDistance, maxCamDistanceFromObj = 0.2f, camMoveVel = 0.05f;
+
+    private Transform camTarget;
+    private Vector3 camPositionVec;
+    private Camera mainCam;
+
+    public bool sideLeft = true;
+    private Vector3 currentCamTargetPos;
+    private float currentCamTargetX;
+
+    // Use this for initialization
+    void Start()
+    {
+        // define currente x on camera target
+        currentCamTargetX = -maxCamDistanceFromObj;
+
+        // find mainCam & camTarget on the objToFollow childs
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        camTarget = objToFollow.transform.Find("camTarget").transform;
+
+        //adjust camTarget to left
+        //use a vector to keep track of camTarget Position
+        currentCamTargetPos = new Vector3(currentCamTargetX, camTarget.localPosition.y, camTarget.localPosition.z);
+        camTarget.localPosition = currentCamTargetPos;
+
+        // calculate the vector that puts the cam on position
+        camPositionVec = -objToFollow.transform.forward * camDistance;
+
+
+        // set position and lookAt for the cam
+        mainCam.transform.position = camTarget.position + camPositionVec;
+        mainCam.transform.LookAt(camTarget);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //target position function
+        camSide();
+
+        // update the vector that puts the cam on position
+        camPositionVec = -objToFollow.transform.forward * camDistance;
+        //!!Remake
+        mainCam.transform.position = camTarget.position + camPositionVec;
+        // update lookAt for the cam
+        mainCam.transform.LookAt(camTarget);
+    }
+
+    private void camSide()
+    {
+        // change cam from shoulder
+        if (Input.GetKeyDown(KeyCode.Q))
+            sideLeft = true;
+        else if (Input.GetKeyDown(KeyCode.E))
+            sideLeft = false;
+
+        // move cam to the left shoulder
+        //on the left side
+        if (sideLeft)
+        {
+            if (currentCamTargetX - camMoveVel * Time.deltaTime < -maxCamDistanceFromObj)
+                currentCamTargetX = -maxCamDistanceFromObj;
+            else
+                currentCamTargetX -= camMoveVel * Time.deltaTime;
+        }
+        //on the right side
+        else if (!sideLeft)
+        {
+            if (currentCamTargetX + camMoveVel * Time.deltaTime > maxCamDistanceFromObj)
+                currentCamTargetX = maxCamDistanceFromObj;
+            else
+                currentCamTargetX += camMoveVel * Time.deltaTime;
+        }
+
+        currentCamTargetPos.x = currentCamTargetX;
+        camTarget.localPosition = currentCamTargetPos;
+    }
+}
---
+++ Projectos/testMechanics_Proto/Assets/_scripts/playerMove.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+
+    public float animAcelaration = 0.04f;
+    public float angularVel = 10.0f;
+
+    private Animator playerAnim;
+    private float locSpeed = 0.5f;
+    private CharacterController cc;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerAnim = GetComponent<Animator>();
+        cc = GetComponent<CharacterController>();
+        playerAnim.applyRootMotion = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+
+        //if w  is pressed
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            playerAnim.SetBool("moving", true);
+
+            // if left shift is pressed
+            if (Input.GetAxis("Fire3") > 0.0f)
+            {
+                if (locSpeed < 1.0f)
+                    locSpeed += animAcelaration;
+                else
+                    locSpeed = 1.0f;
+            }
+            else
+            {
+                if (locSpeed > 0.5f)
+                    locSpeed -= animAcelaration;
+                else
+                    locSpeed = 0.5f;
+            }
+        }
+
+        // if s is pressed
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            playerAnim.SetBool("moving", true);
+            if (locSpeed > 0)
+                locSpeed -= animAcelaration;
+            else
+                locSpeed = 0.0f;
+        }
+
+        //if no vertical buttons are pressed
+        else if (Input.GetAxis("Vertical") == 0)
+        {
+            playerAnim.SetBool("moving", false);
+
+            if (locSpeed < 0.5f && locSpeed + animAcelaration > 0.5f)
+                locSpeed = 0.5f;
+            else if (locSpeed < 0.5f && locSpeed + animAcelaration < 0.5f)
+                locSpeed += animAcelaration;
+            else if (locSpeed > 0.5f && locSpeed - animAcelaration < 0.5f)
+                locSpeed = 0.5f;
+            else if (locSpeed > 0.5f && locSpeed - animAcelaration > 0.5f)
+                locSpeed -= animAcelaration;
+        }
+
+        // set LocSpeed
+        playerAnim.SetFloat("LocSpeed", locSpeed);
+
+        if (locSpeed == 0.5f && playerAnim.GetBool("moving"))
+            if (Input.GetAxis("Horizontal") > 0)
+                this.transform.Rotate(this.transform.up, angularVel);
+            else if (Input.GetAxis("Horizontal") < 0)
+                this.transform.Rotate(this.transform.up, -angularVel);
+
+
+        if (cc.isGrounded && Input.GetKeyDown(KeyCode.Space)) { }
+            
+    }
+
+}
