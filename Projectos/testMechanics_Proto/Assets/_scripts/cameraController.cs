73f8d5ab45193fae3617c3594965ed4c073419da
Unknown <euricomartinss@gmail.com>
2018-04-24 00:12:00 +0100

    update

--- Projectos/testMechanics_Proto/Assets/_scripts/cameraController.cs
+++ Projectos/testMechanics_Proto/Assets/_scripts/cameraController.cs
@@ -10,6 +10,7 @@ public class cameraController : MonoBehaviour
     public float camDistance, maxCamDistanceFromObj = 0.2f, camMoveVel = 0.05f;
     public float anglePerSec = 5.0f;
     public float RunDistanceMulti = 1.1f;
+    public float minDistance = 1.0f, maxDistance = 10.0f;
 
     // on run
     private Animator objToFollowAnim;
@@ -20,6 +21,7 @@ public class cameraController : MonoBehaviour
     private Vector3 camPositionVec;
     private Camera mainCam;
     private Vector3 targetRotationVec;
+    private float distance;
 
     // side
     public bool sideLeft = true;
@@ -27,9 +29,9 @@ public class cameraController : MonoBehaviour
     private float currentCamTargetX;
 
     //collision
-    private bool isColliding = false;
-    private Collision coll;
-
+    private bool iscolliding = false;
+    private Vector3 leftViewPortPos, leftViewPortDir;
+    private Vector3 rightViewPortPos, rightViewPortDir;
 
     void Start()
     {
@@ -54,7 +56,6 @@ public class cameraController : MonoBehaviour
         // set position and lookAt for the cam
         mainCam.transform.position = camTarget.position + camPositionVec;
         mainCam.transform.LookAt(camTarget);
-
     }
 
     // Update is called once per frame
@@ -67,6 +68,9 @@ public class cameraController : MonoBehaviour
 
         // update set the new position and lookAt for the cam
         mainCam.transform.position = camTarget.position + camPositionVec;
+
+        camColl();
+
         mainCam.transform.LookAt(camTarget);
 
         //Debug
@@ -104,30 +108,28 @@ public class cameraController : MonoBehaviour
         camTarget.localPosition = currentCamTargetPos;
     }
 
-    //camera rotation debug
-    private void camDebug()
-    {
-        Debug.DrawRay(objToFollow.transform.position, -objToFollow.transform.forward, Color.red);
-        Debug.DrawRay(objToFollow.transform.position, camPositionVec, Color.green);
 
-    }
 
     private void camAjust()
     {
         // update the vector that puts the cam on position
         targetRotationVec = -objToFollow.transform.forward * camDistance;
+
         // test the angle between old camPostion and the target one, smooth transition
         if (Vector3.Angle(targetRotationVec, -mainCam.transform.forward) > 0.5f &&
             Vector3.Angle(targetRotationVec, -mainCam.transform.forward) <= 30.0f)
         {
-            // using croos funcition can identify if new target is on the left or right
+            //if camera need to rotate right
             if (Vector3.Cross(targetRotationVec, -mainCam.transform.forward).y > 0.0f)
                 camPositionVec = Quaternion.AngleAxis(-anglePerSec * Time.deltaTime, mainCam.transform.up) * camPositionVec;
+            //if camera need to rotate left
             else if (Vector3.Cross(targetRotationVec, -mainCam.transform.forward).y < 0.0f)
                 camPositionVec = Quaternion.AngleAxis(anglePerSec * Time.deltaTime, mainCam.transform.up) * camPositionVec;
+
         }
+
         //limit max angle offset to 30º
-        else if (Vector3.Angle(targetRotationVec, -mainCam.transform.forward) > 30.0f)
+        if (Vector3.Angle(targetRotationVec, -mainCam.transform.forward) > 30.0f)
         {
             if (Vector3.Cross(targetRotationVec, -mainCam.transform.forward).y > 0.0f)
                 camPositionVec = Quaternion.AngleAxis(29.0f, mainCam.transform.up) * targetRotationVec;
@@ -140,52 +142,61 @@ public class cameraController : MonoBehaviour
         {
             //if camera distance is less than running offset
             if (onRunCurrentDistance < camDistance * RunDistanceMulti)
-                onRunCurrentDistance += camMoveVel * 0.5f * Time.deltaTime;
+                onRunCurrentDistance += camMoveVel * 0.2f * Time.deltaTime;
             else
                 onRunCurrentDistance = camDistance * RunDistanceMulti;
         }
         else // not running camera ajust
         {
             if (onRunCurrentDistance > 0.0f)
-                onRunCurrentDistance -= camMoveVel * 0.5f * Time.deltaTime;
+                onRunCurrentDistance -= camMoveVel * 0.2f * Time.deltaTime;
             else
                 onRunCurrentDistance = 0.0f;
         }
 
 
         //set camPositionVec distance to camDistance
-        camPositionVec = camPositionVec.normalized * (camDistance + onRunCurrentDistance);
-        camCollision();
+        distance = Mathf.Clamp(camDistance + onRunCurrentDistance, minDistance, maxDistance);
+        camPositionVec = camPositionVec.normalized * distance;
+
     }
 
-    // camera collision
-    private void camCollision()
+    private void camColl()
     {
-        if (isColliding)
+        LayerMask mask = ~(1 << 8);
+        RaycastHit hit;
+        // left direction and position to the viewport
+        leftViewPortPos = mainCam.ViewportToWorldPoint(new Vector3(0.0f, 0.5f, mainCam.nearClipPlane));
+        leftViewPortDir = (leftViewPortPos - mainCam.transform.position) * 2.0f;
+        // right direction and position to the viewport
+        rightViewPortPos = mainCam.ViewportToWorldPoint(new Vector3(1.0f, 0.5f, mainCam.nearClipPlane));
+        rightViewPortDir = (rightViewPortPos - mainCam.transform.position) * 2.0f;
+
+        //in front of camera
+        if (Physics.Linecast(camTarget.position, mainCam.transform.position, out hit, mask))
         {
-            
+            iscolliding = true;
+
         }
-    }
-
-
-    private void OnCollisionStay(Collision collision)
-    {
-        isColliding = true;
-        coll = collision;
-        Debug.DrawRay(coll.contacts[0].point, Vector3.up, Color.blue);
+        else if (Physics.Linecast(leftViewPortPos, leftViewPortPos + leftViewPortDir, out hit, mask))
+        {
+            iscolliding = true;
+        }
+        else
+            iscolliding = false;
 
     }
-    private void OnCollisionEnter(Collision collision)
-    {
-        isColliding = true;
-        coll = collision;
-        Debug.DrawRay(coll.contacts[0].point, Vector3.up, Color.blue);
 
-
-    }
-    private void OnCollisionExit(Collision collision)
+    //camera rotation debug
+    private void camDebug()
     {
-        isColliding = false;
-        coll = null;
+        //rotation
+        Debug.DrawRay(objToFollow.transform.position, -objToFollow.transform.forward, Color.red);
+        Debug.DrawRay(objToFollow.transform.position, camPositionVec, Color.green);
+
+        //collision
+        Debug.DrawLine(leftViewPortPos, leftViewPortPos + leftViewPortDir, Color.blue);   //left
+        Debug.DrawLine(rightViewPortPos, rightViewPortPos + rightViewPortDir, Color.blue);   //right
+        Debug.DrawLine(camTarget.position, mainCam.transform.position, Color.red);  //back
     }
 }
