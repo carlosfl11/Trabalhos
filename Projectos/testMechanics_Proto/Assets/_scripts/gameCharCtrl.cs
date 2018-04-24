3c6d72ebfb486cc03c855b87e490141a3e76b823
Unknown <euricomartinss@gmail.com>
2018-04-24 23:55:38 +0100

    modelSwitchSystem(start)

---
+++ Projectos/testMechanics_Proto/Assets/_scripts/gameCharCtrl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameCharCtrl : MonoBehaviour {
+
+    //Animator
+    private Animator anim;
+    public GameObject player;
+
+    //models
+    public GameObject douglas;
+    public GameObject douglasInf;
+
+    //avatar
+    public Avatar douglasAvatar, douglasInfAvatar;
+
+    //controllers
+    public RuntimeAnimatorController douglasCtrl, douglasInfCtrl;
+
+
+	// Use this for initialization
+	void Start () {
+        anim = player.GetComponent<Animator>();
+
+        anim.avatar = douglasAvatar;
+        anim.runtimeAnimatorController = douglasCtrl;
+
+        douglas.SetActive(true);
+        douglasInf.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
